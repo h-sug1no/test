b84c57e3d7d181e4cf374927eedfcb8f6f84014b
h-sug1no <59550999+h-sug1no@users.noreply.github.com>
2022-01-26 12:05:54 +0900

    [WIP]: add play(skipSilence). render current play pos marker.

--- audio-view-test/main.js
+++ audio-view-test/main.js
@@ -7,6 +7,8 @@
   let c2d;
   let c2dElm;
   let logElm;
+  let silences = [];
+  let activeSource;
 
   const render = (timestamp=0) => {
     if (!c2d) {
@@ -48,14 +50,33 @@
           const data = buffer.getChannelData(i);
           texts.push(`data[${i}]: length=${data.length}`);
         }
+        texts.push(`silences: ${JSON.stringify(silences, null, 2)}`);
       }
       logElm.textContent = texts.join('\n');
+    }
+
+    if (c2d) {
+      c2d.clearRect(0, 0, clientWidth, 50);
+      c2d.fillStyle = 'black';
+      c2d.fillText(activeSource ? 'playing' : '', 0, 20);
+      if (buffer && actx && activeSource) {
+        c2d.fillRect(clientWidth *
+          ((activeSource.offset +
+            (actx.currentTime - activeSource.startTime)) / buffer.duration),
+        20, 1, 30);
+      }
     }
 
     if (dirty && c2d) {
       c2d.clearRect(0, 0, clientWidth, clientHeight)
       // c2d.globalCompositeOperation = 'xor';
-
+      let dataLength = -Infinity;
+      for (let i = 0; i < buffer.numberOfChannels; i += 1) {
+        const data = buffer.getChannelData(i);
+        dataLength = Math.max(dataLength, data.length);
+      }
+      const sampleSec = buffer.duration / dataLength;
+      silences = [];
       for (let i = 0; i < buffer.numberOfChannels; i += 1) {
         const row = clientHeight * (1 / (buffer.numberOfChannels + 1));
         const y = row * (i + 1);
@@ -63,15 +84,33 @@
         c2d.fillRect(0, y, clientWidth, 1);
         const data = buffer.getChannelData(i);
 
-        c2d.fillStyle = `hsl(${360/10*i}, 80%, 60%)`;
-
         data.forEach((v, idx) => {
           const x = (clientWidth / data.length) * idx;
+          c2d.fillStyle = `hsl(${360/10*i}, 80%, 60%)`;
           c2d.fillRect(x, y, 1 , v * hRow);
           if (Math.abs(v) > 1) {
             console.log(idx, v);
           }
-            
+          if (Math.abs(v) <= 0.01) {
+            if (!silences[i] || !silences[i].done) {
+              silences[i] = {
+                sec: sampleSec * idx,
+                idx,
+                v,
+              };
+            }
+          } else if (silences[i]) {
+            if (!silences[i].done) {
+              silences[i].done = true;
+              silences[i].end = {
+                idx,
+                v,
+                sec: sampleSec * idx,
+              }
+              c2d.fillStyle = 'black';
+              c2d.fillRect(x, y, 1, hRow);
+            }
+          }
         });
       }
     }
@@ -87,12 +126,34 @@
     });
 
     let source;
-  window.AUDIOVIEW = {
-    play() {
+    window.AUDIOVIEW = {
+    play(skipSilence) {
+      if (activeSource) {
+        return;
+      }
       source = actx.createBufferSource();
       source.buffer = buffer;
       source.connect(actx.destination);
-      source.start(0);
+      let offset = 0;
+      if (skipSilence) {
+        offset = Infinity;
+        silences.forEach((s) => {
+          if (s.end) {
+            offset = Math.min(offset, s.end.sec);
+          }
+        });
+      }
+      source.onended = () => {
+        activeSource = undefined;
+      };
+      activeSource = {
+        source,
+        startTime: actx.currentTime + 0.001,
+        offset,
+      };
+
+      source.start(activeSource.startTime,
+        offset);
     },
     stop() {
       if (source) {
