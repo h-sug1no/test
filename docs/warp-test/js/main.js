fd943e38bbb3d81965b060a620f6477b83a1463b
h-sug1no <59550999+h-sug1no@users.noreply.github.com>
2023-01-31 07:01:50 +0900

    [WIP]: add test page (on going).

---
+++ docs/warp-test/js/main.js
@@ -0,0 +1,16 @@
+(()=>{
+
+  const sps = new URLSearchParams(location.search);
+
+
+  const init=()=>{
+    const audioElm = document.querySelector('#player');
+    audioElm.src = sps.get('src');
+    window.WTGLOBAL.audioElm = audioElm;
+  }
+
+
+  window.WTGLOBAL = {
+    init,
+  }
+})();
